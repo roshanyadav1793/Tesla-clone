e8282da0581fadbab3574ea52b15b48310997702
Roshan <roshanyadav1793@gmail.com>
2021-12-05 23:19:44 +0530

    Final change

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,12 +12,47 @@ function Home() {
                 leftBtnText="Custom Order"
                 rightBtnText="Existing Inventory"
             />
+            <Section
+                title= "Model Y"
+                description= "Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title= "Model 3"
+                description= "Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title= "Model X"
+                description= "Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
             <Section 
                 title="Lowest Cost solar panels in America"
                 description="Money-Back Gurantee"
                 backgroundImg="solar-panel.jpg"
                 leftBtnText="Order Now"
                 rightBtnText="Learn More"
+            />
+            <Section 
+                title="Solar for new roofs"
+                description="Solar roof costs less thana normal roof"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section 
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Order Now"
+                rightBtnText=""
             />
         </Container>
     )
